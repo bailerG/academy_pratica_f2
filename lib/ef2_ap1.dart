60cc804a110da8559e769372e4bd52aea4296cb2
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-19 15:14:12 -0300

    AP1 Concluida

--- lib/ef2_ap1.dart
+++ lib/ef2_ap1.dart
@@ -1,3 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(8),
+                width: 50,
+                height: 50,
+                color: Colors.red,
+              ),
+              Container(
+                margin: const EdgeInsets.all(8),
+                width: 50,
+                height: 50,
+                color: Colors.green,
+              ),
+              Container(
+                margin: const EdgeInsets.all(8),
+                width: 50,
+                height: 50,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          Container(
+            color: Colors.yellow,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  width: 50,
+                  height: 80,
+                  color: Colors.deepPurple,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  width: 50,
+                  height: 80,
+                  color: Colors.cyan,
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(
+                            bottom: 8,
+                          ),
+                          width: 50,
+                          height: 50,
+                          color: Colors.deepPurple,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.cyan,
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 80,
+            margin: const EdgeInsets.all(8),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.black,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+          )
+        ]));
+  }
+}
