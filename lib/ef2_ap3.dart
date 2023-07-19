11b8b03c2a95a5494676c580474b903c23a1e4cf
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-19 17:34:43 -0300

    AP3 Concluida

--- lib/ef2_ap3.dart
+++ lib/ef2_ap3.dart
@@ -1,12 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +18,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: darkBlue,
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: const Scaffold(
         body: Center(
           child: MyWidget(),
         ),
@@ -24,16 +28,65 @@ class MyApp extends StatelessWidget {
 }
 
 class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
   @override
   State<MyWidget> createState() => _MyWidgetState();
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  final random = Random();
+  var quadrado = true;
+  var cor = const Color.fromRGBO(244, 67, 54, 1);
+
+  String get textoBotao =>
+      quadrado ? "Mudar para circulo" : "Mudar para quadrado";
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Hello, World!',
-      style: Theme.of(context).textTheme.headlineMedium,
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(8),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      quadrado = !quadrado;
+                    });
+                  },
+                  child: Text(textoBotao),
+                )),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    var r = random.nextInt(255);
+                    var g = random.nextInt(255);
+                    var b = random.nextInt(255);
+                    cor = Color.fromARGB(255, r, g, b);
+                  });
+                },
+                child: const Text('Mudar cor'))
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: cor,
+                    shape: quadrado ? BoxShape.rectangle : BoxShape.circle),
+              ),
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
